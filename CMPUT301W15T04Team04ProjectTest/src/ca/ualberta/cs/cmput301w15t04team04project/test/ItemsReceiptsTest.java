dd85a609e552613c335956b3c020d6f7af8440bd
jeremykid <jeremykid1992@gmail.com>
2015-02-09 16:39:47 -0700

    do half of the receipt test

---
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/ItemsReceiptsTest.java
@@ -0,0 +1,70 @@
+package ca.ualberta.cs.cmput301w15t04team04project.test;
+
+import java.io.File;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import junit.framework.TestCase;
+import ca.ualberta.cs.cmput301w15t04team04project.*;
+
+public class ItemsReceiptsTest extends TestCase {
+
+	Uri imageFileUri;
+	
+    //@Override
+   /* public void onCreate(Bundle savedInstanceState) {
+       
+  
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+  
+    }
+    
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    */
+    public void takeAPhoto() {
+    	
+    }
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    }
+    
+    protected void deleteReceiptTest(){
+    	
+    	
+    }
+    
+    protected void viewReceiptTest(){
+    	
+    
+    }
+    
+    protected void underSizeReceiptTest(){
+    	
+    	
+    }
+    
+    protected void retakenReceiptTest(){
+    	
+        
+    }
+    
+    protected void saveReceiptTest(){
+    	
+    	
+    }
+}
