1c01fac269bc0de1f265749227f3fcd81b6ac545
jeremykid <jeremykid1992@gmail.com>
2015-02-09 16:14:58 -0700

    add project and test

---
+++ CMPUT301W15T04Team04Project/gen/ca/ualberta/cs/cmput301w15t04team04project/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.ualberta.cs.cmput301w15t04team04project;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
