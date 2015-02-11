408a917149270adea514bf1b9adf54db11ac715e
sonicsoy <jyang7@ualberta.ca>
2015-02-11 12:40:08 -0700

    items overall test debugged

--- CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Items_Overall_Test.java
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Items_Overall_Test.java
@@ -7,31 +7,33 @@ import ca.ualberta.cs.cmput301w15t04team04project.Item;
 import junit.framework.TestCase;
 
 public class Items_Overall_Test extends TestCase {
-	 final String itemname = "car";
-	 final String claimname = "Beijing";
-	 public void testItem(){
-		 Item item = new Item(itemname);
-		 item.setStartDate(new Date());
-		 item.setCategary(new String());
-		 item.setDes(new String());
-		 int a = 100;
-		 item.setAmount(a);
-		 item.setUnit(new String());
-	 }
-	 
-	 public void testFlag(){
-		 Item item = new Item(itemname);
-		 item.addFlag();
-		 item.removeFlag();
-	 }
-	 
-	 public void testItemView(){
-		 Item item = new Item(itemname);
-		 System.out.println(item.getStartdate()+item.getCategary()+item.getDes()+item.getAmount()+item.getUnit());
-	 }
-	 
-	 public void testEditDeleteItem(){
-		 Claim claim = new Claim(claimname);
-		 claim.removeItem(new Item(itemname));
-	 }
+	final String itemname = "car";
+	final String claimname = "Beijing";
+
+	public void testItem() {
+		Item item = new Item(itemname);
+		item.setDate(new Date());
+		item.setCategary(new String());
+		item.setDes(new String());
+		int a = 100;
+		item.setAmount(a);
+		item.setUnit(new String());
+	}
+
+	public void testFlag() {
+		Item item = new Item(itemname);
+		item.addFlag();
+		item.removeFlag();
+	}
+
+	public void testItemView() {
+		Item item = new Item(itemname);
+		System.out.println(item.getDate() + item.getCategary() + item.getDes()
+				+ item.getAmount() + item.getUnit());
+	}
+
+	public void testEditDeleteItem() {
+		Claim claim = new Claim(claimname);
+		claim.removeItem(new Item(itemname));
+	}
 }
--- CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Items_Receipts_Test.java
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Items_Receipts_Test.java
@@ -5,55 +5,52 @@ import android.content.Intent;
 import android.net.Uri;
 import junit.framework.TestCase;
 
-
 public class Items_Receipts_Test extends TestCase {
 
 	Uri imageFileUri;
-	
 
-    
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-    }
-    
-    //test delete receipt test
-    protected void deleteReceiptTest(){
-    	Item item = new Item("test");
-    	item.takeAPhoto();
-    	assertTrue("exist photo", item.getPhoto()!=null);
-    	item.deleteAPhoto();
-    	assertTrue("delete a photo", item.getPhoto()==null);
-    }
-    
-    protected void viewReceiptTest(){
-    	Item item = new Item("test");
-    	item.takeAPhoto();
-    	assertTrue("view Receipt",item.getPhoto().equals(imageFileUri));
-    	
-    }
-    
-    protected void underSizeReceiptTest(){
-     	Item item = new Item("test");
-    	item.takeAPhoto();
-    	
-    	assertTrue("undersize", item.getPhotoSize()<=65536);
-    }
-    
-    protected void retakenReceiptTest(){
-    	Item item = new Item("test");
-    	item.takeAPhoto();
-    	assertTrue("exist photo", item.getPhoto()!=null);
-    	item.deleteAPhoto();
-    	assertTrue("delete a photo", item.getPhoto()==null);
-    	item.takeAPhoto();
-    	assertTrue("exist photo", item.getPhoto()!=null);
-    }
-    
-    protected void saveReceiptTest(){
-    	Item item = new Item("test");
-    	item.takeAPhoto();
-    	assertTrue("exist photo", item.getPhoto()!=null);
-    	
-    	//question 
-    }
+	}
+
+	// test delete receipt test
+	protected void deleteReceiptTest() {
+		Item item = new Item("test");
+		item.takeAPhoto();
+		assertTrue("exist photo", item.getPhoto() != null);
+		item.deleteAPhoto();
+		assertTrue("delete a photo", item.getPhoto() == null);
+	}
+
+	protected void viewReceiptTest() {
+		Item item = new Item("test");
+		item.takeAPhoto();
+		assertTrue("view Receipt", item.getPhoto().equals(imageFileUri));
+
+	}
+
+	protected void underSizeReceiptTest() {
+		Item item = new Item("test");
+		item.takeAPhoto();
+
+		assertTrue("undersize", item.getPhotoSize() <= 65536);
+	}
+
+	protected void retakenReceiptTest() {
+		Item item = new Item("test");
+		item.takeAPhoto();
+		assertTrue("exist photo", item.getPhoto() != null);
+		item.deleteAPhoto();
+		assertTrue("delete a photo", item.getPhoto() == null);
+		item.takeAPhoto();
+		assertTrue("exist photo", item.getPhoto() != null);
+	}
+
+	protected void saveReceiptTest() {
+		Item item = new Item("test");
+		item.takeAPhoto();
+		assertTrue("exist photo", item.getPhoto() != null);
+
+		// question
+	}
 }
