18951de356eacdae1e7d22411cc8d15d97450383
sonicsoy <jyang7@ualberta.ca>
2015-02-12 14:02:31 -0700

    check

--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/Claim.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/Claim.java
@@ -13,12 +13,10 @@ public class Claim {
 	protected ArrayList<Tag> tagList;
 	protected ArrayList<Destination> destinations;
 	protected ArrayList<Listener> listener;
-<<<<<<< HEAD
-	protected Approval approver;
-=======
 
-	protected String approver;
->>>>>>> 9308e71a7581672875e86432481aecd0421c80fe
+	protected Approval approver;
+
+
 	protected boolean editable = true;
 
 	public Claim(String claimname) {
@@ -26,15 +24,10 @@ public class Claim {
 
 	}
 
-	public String getApprover() {
-		return approver;
-	}
 
-	public void setApproval(Approval approver) {
-		
-	}
 
-	public void removeItem(Expense item) {
+
+	public void removeItem(Item item) {
 		// TODO Auto-generated method stub
 
 	}
@@ -115,5 +108,27 @@ public class Claim {
 	public Item getItem(int i) {
 		return itemList.get(i);
 		
+	}
+
+
+	public void setApprover(Approval approval) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	public Approval getApprover() {
+		// TODO Auto-generated method stub
+		return approver;
+	}
+
+
+
+
+	public ArrayList<Claim> getItemList() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
