d8f88144610049f729ead898a7b6668288a85254
Youdong Ma <youdong@cs.ualberta.ca>
2015-02-13 11:24:17 -0700

    status

--- CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Claims_Status_Test.java
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Claims_Status_Test.java
@@ -19,18 +19,24 @@ public class Claims_Status_Test extends
 	claimList.addClaim(claim);
 	public void testSubmitApprover() {
 		((Button) activity.findViewById(ca.ualberta.cs.cmput301w15t04team04project.R.id.submitClaim)).performClick();
-		assertTrue("A claim with submitted status should not be editable",claim.getStatus().equals("submitted"));
+		assertTrue("A claim with submitted status?",claim.getStatus().equals("submitted"));
      	assertTrue("A claim with submitted status should not be editable",claim.getEditable() == false);
 	}
 
 	public void ClaimStatusChanged() {
 		((Button) activity.findViewById(ca.ualberta.cs.cmput301w15t04team04project.R.id.returnClaim)).performClick();
-		assertTrue("A claim with submitted status should not be editable",claim.getStatus().equals("returned"));
-     	assertTrue("A claim with submitted status should not be editable",claim.getEditable() == true);
+		assertTrue("A claim with returned status?",claim.getStatus().equals("returned"));
+     	assertTrue("A claim with returned status should be editable",claim.getEditable() == true);
 		((Button) activity.findViewById(ca.ualberta.cs.cmput301w15t04team04project.R.id.approveClaim)).performClick();
-		assertTrue("A claim with submitted status should not be editable",claim.getStatus().equals("approved"));
-	    assertTrue("A claim with submitted status should not be editable",claim.getEditable() == false);
+		assertTrue("A claim with approved status?",claim.getStatus().equals("approved"));
+	    assertTrue("A claim with approved status should not be editable",claim.getEditable() == false);
 
 	}
 
+	public void showFeedback(){
+		Approval approver = new Approval("jack");
+		approver.approve(claim, "This a good claim");
+		assertTrue("recieve feedback?",claim.getApprover() == "jack");
+		assertTrue("recieve feedback?",claim.getComment() == "This a good claim");
+	}
 }
