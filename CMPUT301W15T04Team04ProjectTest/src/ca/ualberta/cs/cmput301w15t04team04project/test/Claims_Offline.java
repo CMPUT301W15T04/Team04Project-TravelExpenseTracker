8da5f7cbedc364b1b9cb32c99f58ceab38bd4f26
Youdong Ma <youdong@cs.ualberta.ca>
2015-02-23 11:20:05 -0700

    us09

--- CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Claims_Offline.java
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Claims_Offline.java
@@ -1,13 +1,39 @@
 package ca.ualberta.cs.cmput301w15t04team04project.test;
 
+import ca.ualberta.cs.cmput301w15t04team04project.ClaimList;
+import ca.ualberta.cs.cmput301w15t04team04project.Controller;
+import ca.ualberta.cs.cmput301w15t04team04project.Manager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class Claims_Offline extends ActivityInstrumentationTestCase2<T> {
-
-	
-	
+	ClaimList claimlist = Manager.getClaimList();
+	ConnectivityManager connectManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	NetworkInfo activeNetworkInfo = connectManager
+			.getActiveNetworkInfo();
 	protected void setUp() throws Exception {
 		super.setUp();
+		
+	}
+	
+	/*
+	 *  US09.01.01
+	 */
+	
+	public void testReadOnlineInfo() {
+		
+		ClaimList claimListFromOnline;
+		assertTrue(isNetworkAvailable());
+		Controller.SaveToOnline();
+		claimListFromOnline = Controller.LoadFromOnline();
+		assertEquals("was ClaimList succefully written to db?",
+				claimlist.toString(), claimListFromOnline.toString());
 	}
 
+	// http://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
+	public boolean isNetworkAvailable() {
+		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
 }
