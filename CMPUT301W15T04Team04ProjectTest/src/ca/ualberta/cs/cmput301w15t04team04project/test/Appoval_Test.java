928789ad649da6b86a602819e2dc53911473582b
jeremykid <jeremykid1992@gmail.com>
2015-02-23 11:37:15 -0700

    Ui test for approverl

--- CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Appoval_Test.java
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Appoval_Test.java
@@ -3,9 +3,12 @@ package ca.ualberta.cs.cmput301w15t04team04project.test;
 import java.util.ArrayList;
 import java.util.Date;
 
+import android.app.Activity;
 import android.app.Instrumentation.ActivityMonitor;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
@@ -22,12 +25,34 @@ import ca.ualberta.cs.cmput301w15t04team04project.Claimant;
 import ca.ualberta.cs.cmput301w15t04team04project.Destination;
 import ca.ualberta.cs.cmput301w15t04team04project.Item;
 import ca.ualberta.cs.cmput301w15t04team04project.MainActivity;
+import ca.ualberta.cs.cmput301w15t04team04project.Manager;
 import ca.ualberta.cs.cmput301w15t04team04project.OneClaimActivity;
 //import ca.ualberta.cs.lonelytwitter.IntentReaderActivity;
 
 
 public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimActivity>   {
+	public Appoval_Test(Class<AddEditClaimActivity> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
 
+	Activity activity;
+	ClaimList claimList = Manager.getClaimList();
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		// add a claim to test on
+		Claim claim = new Claim("Test");
+		claim.setStartDate(new Date());
+		Item item = new Item("Item1");
+		claim.addItem(item);
+		claimList.addClaim(claim);
+		Intent intent = new Intent();
+		intent.putExtra("Index", 0);
+		setActivityIntent(intent);
+		activity = getActivity();
+	}
+	
 	//US08.01.01
 	protected void viewSubmittedClaimsTest() {
 		
@@ -50,6 +75,18 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 		
 		testOpenNextActivity(MainActivity, AddEditClaimActivity);
 		
+		final Button button = (Button) activity
+				.findViewById(ca.ualberta.cs.cmput301w15t04team04project.R.id.AddEditClaimViewSubmittedListButton);
+		activity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				// click button and open next activity.
+				button.performClick();
+			}
+		});
+		View v = activity.getWindow().getDecorView()
+				.findViewById(android.R.id.MainShowSubmittedListView);
+		assertTrue("Toast is shown", v.isShown());
 		
 	}
 	
@@ -119,7 +156,7 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 		assertFalse("is photo", itemB.getPhoto().equals(null));
 		
 		testOpenNextActivity(OneClaimActivity, AddEditExpenseActivity);
-
+		
 	}
 	
 	//US08.02.01
@@ -145,11 +182,31 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 				.get(0).getItemList().get(0).equals(itemA));
 
 		testOpenNextActivity(MainActivity, AddEditClaimActivity);
-
+		View v = activity.getWindow().getDecorView()
+				.findViewById(android.R.id.MainClaimListView);
+		assertTrue("Toast is shown", v.isShown());
 
 	}
 
+	
+	/*
+	
+	//should go to another activity AddEditClaimActivity
+	
+	public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimActivity>   {
+	public Appoval_Test(Class<AddEditClaimActivity> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+	Activity activity;
+	ClaimList claimList = Manager.getClaimList();
+	
+	*/
+	
 	// US08.06.01
+	
+	
 	protected void addACommentOfSubmittedClaimTest() {
 
 		ClaimList testClaimList = new ClaimList();
@@ -167,9 +224,24 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 		assertEquals("comment added ", AClaim.getApprover().getComment().toString(), "comment test");
 		
 		testOpenNextActivity(MainActivity, AddEditClaimActivity);
-
+		
+		TextView v = (TextView) activity.getWindow().getDecorView()
+				.findViewById(android.R.id.);
+		assertTrue("Toast is shown", v.isShown());
 	}
 
+	//from us07.05.01
+	public void showFeedback() {
+		Approval approver = new Approval("jack");
+		Claim claim = claimList.getPosition(0);
+		approver.approve(claim, "This a good claim");
+		assertTrue("recieve feedback?", claim.getApprover() == "jack");
+		assertTrue("recieve feedback?",
+				claim.getComment() == "This a good claim");
+		TextView view = (TextView) activity.findViewById(R.id.commentsForClaim);
+		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(), view);
+	}
+	
 	//US08.07.01
 	protected void returnClaimNotApproveTest() {
 
@@ -191,7 +263,20 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 		assertEquals("status is return? ", BClaim.getStatus(), "Returned");
 		
 		testOpenNextActivity(MainActivity, AddEditClaimActivity);
-
+		
+		
+		final Button button = (Button) activity
+				.findViewById(ca.ualberta.cs.cmput301w15t04team04project.R.id.MainViewNotApprovedClaimListButton);
+		activity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				// click button and open next activity.
+				button.performClick();
+			}
+		});
+		View v = activity.getWindow().getDecorView()
+				.findViewById(android.R.id.MainShowNotApprovedListView);
+		assertTrue("Toast is shown", v.isShown());
 	}
 
 	//US08.08.01
@@ -213,7 +298,8 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 				BClaim.getApprover().getName().equals("tester"));
 		testOpenNextActivity(MainActivity, AddEditClaimActivity);
 
-
+		TextView view = (TextView) activity.findViewById(R.id.AddEditClaimApproverNameTextView);
+		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(), view);
 	}
 
 	protected void approveClaimSetApproverNameTest() {
@@ -235,7 +321,8 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 				BClaim.getApprover().getName().equals("tester"));
 		testOpenNextActivity(MainActivity, AddEditClaimActivity);
 
-
+		TextView view = (TextView) activity.findViewById(R.id.AddEditClaimApproverNameTextView);
+		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(), view);
 	}
 	
 	//US08.09.01 added 2015-02-12
@@ -257,10 +344,14 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<AddEditClaimA
 		assertEquals("should not changed", claimA.getStatus(), "Submitted");
 		testOpenNextActivity(MainActivity, AddEditClaimActivity);
 
-
+		assertTrue("Claimant cannot edit the claim write by himself",
+				claimA.getEditable() == false);
 		
 		
 	}
+	
+
+
 	
 	//taken from http://stackoverflow.com/questions/9405561/test-if-a-button-starts-a-new-activity-in-android-junit-pref-without-robotium
 	public void testOpenNextActivity(Activity myAcitivity, Activity nextActivity) {
