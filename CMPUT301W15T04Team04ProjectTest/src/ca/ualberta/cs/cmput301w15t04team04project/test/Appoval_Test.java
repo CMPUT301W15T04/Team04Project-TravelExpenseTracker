f207a32bc1adda6957c36c38c7d9880eb482e722
jeremykid <jeremykid1992@gmail.com>
2015-03-04 20:57:20 -0700

    just a little about the uml and the test for 08.02.01

--- CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Appoval_Test.java
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/Appoval_Test.java
@@ -303,33 +303,6 @@ public class Appoval_Test extends ActivityInstrumentationTestCase2<SignInActivit
 	}
 	
 	//US08.02.01
-	protected void sortedClaimListTest() {
-		ClaimList testClaimList = new ClaimList();
-		Claim AClaim = new Claim("A");
-		Claim BClaim = new Claim("B");
-		Claim CClaim = new Claim("C");
-		testClaimList.addClaim(AClaim);
-		testClaimList.addClaim(BClaim);
-		testClaimList.addClaim(CClaim);
-		AClaim.setStatus("Submitted");
-		BClaim.setStatus("Submitted");
-		AClaim.setClaimName("1");
-
-		Item itemA = new Item("food");
-		Item itemB = new Item("texi");
-		AClaim.addItem(itemA);
-		BClaim.addItem(itemB);
-		assertTrue("first item is true", testClaimList.getSubmittedClaimList()
-				.get(1).getItemList().get(0).equals(itemB));
-		assertTrue("first item is true", testClaimList.getSubmittedClaimList()
-				.get(0).getItemList().get(0).equals(itemA));
-
-		testOpenNextActivity(MainActivity, AddEditClaimActivity);
-		View v = activity.getWindow().getDecorView()
-				.findViewById(android.R.id.MainClaimListView);
-		assertTrue("Toast is shown", v.isShown());
-
-	}
 
 	
 	/*
