8ae5c0ec1a2f19cdcfdcf32395b27e74b5e4d793
mayoudong1993 <youdong@ualberta.ca>
2015-03-10 12:49:34 -0600

    right

--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/View/XListView.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/View/XListView.java
@@ -1,3 +1,23 @@
+/*
+* Copyright 2015 Weijie Sun
+* Copyright 2015 Youdong Ma
+* Copyright 2015 Yufei Zhang
+* Copyright 2015 Chenrui Lei
+* Copyright 2015 Yang Zhang
+* Copyright 2015 Ji Yang
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 /**
  * @file XListView.java
  * @package me.maxwin.view
@@ -7,7 +27,7 @@
  * 		Implement IXListViewListener, and see stopRefresh() / stopLoadMore().
  */
 package ca.ualberta.cs.cmput301w15t04team04project.View;
-
+//https://github.com/mayoudong1993/XListView-Android 2015-03-10
 import ca.ualberta.cs.cmput301w15t04team04project.R;
 import android.content.Context;
 import android.util.AttributeSet;
