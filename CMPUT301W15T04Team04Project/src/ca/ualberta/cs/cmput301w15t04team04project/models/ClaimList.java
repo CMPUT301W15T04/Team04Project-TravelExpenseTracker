f5c6889c8510e47646e6711c408560d4d4e4c22a
sonicsoy <jyang7@ualberta.ca>
2015-03-09 20:07:29 -0600

    javadoc of claimList class

--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/models/ClaimList.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/models/ClaimList.java
@@ -4,25 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ca.ualberta.cs.cmput301w15t04team04project.models.Claim;
+
 /**
-* The ClaimList model is design for store Claim objects in order to be used by the view and modified by the controller. 
-*
+ * The ClaimList model is design for store Claim objects in order to be used by
+ * the view and modified by the controller.
+ *
  * @author Ji Yang
  * @author Yang Zhang
  * @version 1.1
  * @since 2015-03-09
-*/
+ */
 
 public class ClaimList {
 
 	protected ArrayList<Claim> claimList = null;
 	protected transient ArrayList<Listener> listeners = null;
 
+	/**
+	 * The constructor of the class
+	 * 
+	 * 
+	 */
 	public ClaimList() {
 		claimList = new ArrayList<Claim>();
 		listeners = new ArrayList<Listener>();
 	}
 
+	/**
+	 * get the position of the selected claim in the claim list in the view.
+	 * 
+	 * @return claimList.get(position) the position of the selected claim in the
+	 *         claim list in the view.
+	 */
 	public Claim getPosition(int position) {
 		return claimList.get(position);
 	}
@@ -48,21 +61,45 @@ public class ClaimList {
 		}
 	}
 
+	/**
+	 * add an new claim to claimList and notify the listener in order to update
+	 * the view
+	 * 
+	 * @param claim
+	 *            the added claim.
+	 */
 	public void addClaim(Claim claim) {
 		claimList.add(claim);
 		notifyListeners();
 	}
 
+	/**
+	 * remove a claim in the claimList and notify the listener in order to
+	 * update the view
+	 * 
+	 * @param claim
+	 *            the removed claim .
+	 */
 	public void deleteClaim(Claim claim) {
 		claimList.remove(claim);
 		notifyListeners();
 	}
 
-	public List<Claim> getClaim() {
+	/**
+	 * get a claim list in List date type
+	 * 
+	 * @return claimList the claimList in the List type .
+	 */
+	public List<Claim> getClaimList() {
 		return claimList;
 	}
 
-	public ArrayList<Claim> getClaimList() {
+	/**
+	 * get a claim list in ArrayList date type
+	 * 
+	 * @return claimList the claimList in the ArrayList type .
+	 */
+	public ArrayList<Claim> getClaimArrayList() {
 		return claimList;
 	}
 
