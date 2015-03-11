720172d8c9c3fc65e5603361f7d35c8d937001ac
mayoudong1993 <youdong@ualberta.ca>
2015-03-10 19:33:23 -0600

    manager

--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/CLmanager/CLmanager.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/CLmanager/CLmanager.java
@@ -1,78 +1,144 @@
 /*
-* Copyright 2015 Weijie Sun
-* Copyright 2015 Youdong Ma
-* Copyright 2015 Yufei Zhang
-* Copyright 2015 Chenrui Lei
-* Copyright 2015 Yang Zhang
-* Copyright 2015 Ji Yang
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 Weijie Sun
+ * Copyright 2015 Youdong Ma
+ * Copyright 2015 Yufei Zhang
+ * Copyright 2015 Chenrui Lei
+ * Copyright 2015 Yang Zhang
+ * Copyright 2015 Ji Yang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ca.ualberta.cs.cmput301w15t04team04project.CLmanager;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.DefaultClientConnection;
-
+import org.apache.http.entity.StringEntity;
 import ca.ualberta.cs.cmput301w15t04team04project.models.Claim;
 import ca.ualberta.cs.cmput301w15t04team04project.models.ClaimList;
 
 import com.google.gson.Gson;
 
 public class CLmanager {
-	private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301w15t04/claimList/";
-
-	private Gson gson;
+	private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301w15t04/";
+	private Gson gson = new Gson();
+	private static HttpClient httpClient = new DefaultHttpClient();
 
 	public CLmanager() {
 		gson = new Gson();
 	}
 
-	public ClaimList getClaimList() {
-		ClaimList claimList = new ClaimList();
-		return claimList;
+	public void insertClaim(Claim claim, String string)
+			throws IllegalStateException, IOException {
+		HttpPost httpPost = new HttpPost(RESOURCE_URL + string + "/"
+				+ claim.getStartDate().getTime());
+		StringEntity stringEntity = null;
+		try {
+			stringEntity = new StringEntity(gson.toJson(claim));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setEntity(stringEntity);
+		HttpResponse response = null;
+		try {
+			response = httpClient.execute(httpPost);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public static Claim getClaim(long ID) {
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpGet httpGet = new HttpGet(RESOURCE_URL + ID);
-		
-		HttpResponse response;
+	public void deleteClaim(long questionId) {
+		HttpDelete deleteRequest = new HttpDelete(RESOURCE_URL + questionId);
+		deleteRequest.setHeader("Accept", "application/json");
+		HttpResponse deleteResponse = null;
+		try {
+			deleteResponse = httpClient.execute(deleteRequest);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateClaim(Claim claim) throws IllegalStateException,
+			IOException {
+		HttpPost updateRequest = new HttpPost(RESOURCE_URL + "waitingList"
+				+ "/" + claim.getStartDate().getTime());
+		StringEntity stringEntity = null;
+		try {
+			stringEntity = new StringEntity(gson.toJson(claim));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		updateRequest.setHeader("Accept", "application/json");
+		updateRequest.setEntity(stringEntity);
+		HttpResponse postResponse = null;
+		try {
+			postResponse = httpClient.execute(updateRequest);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Claim getClaim(long ID, String string) {
+
+		HttpGet getRequest = new HttpGet(RESOURCE_URL + string + "/" + ID);
+		getRequest.setHeader("Accept", "application/json");
+		HttpResponse getResponse;
 		Claim claim = null;
 		try {
-			response = httpClient.execute(httpGet);
-			
-			
+			getResponse = httpClient.execute(getRequest);
+			// We have to tell GSON what type we expect
+			/*Type elasticSearchResponseType = new TypeToken<ElasticSearchResponse<Recipe>>() {
+			}.getType();
+			// Now we expect to get a Recipe response
+			ElasticSearchResponse<Recipe> esResponse = gson.fromJson(json,
+					elasticSearchResponseType);
+			// We get the recipe from it!
+			claim = esResponse.getSource();*/
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return claim;
 	}
 
-	public ClaimList getQuestionList(ArrayList<Long> listID) {
-		ClaimList cliamList = new ClaimList();
-		for (long ID : listID) {
-			cliamList.addClaim(getClaim(ID));
+	public ClaimList getClaimList(ArrayList<Long> listID, String string) {
+		ClaimList claimList = new ClaimList();
+		for (long Id : listID) {
+			claimList.addClaim(getClaim(Id, string));
 		}
-		return cliamList;
+		return claimList;
 	}
-
-	//private SearchHit<Claim> parseClaimHit(HttpResponse response) {
-		// TODO Auto-generated method stub
-	//	return null;
-	//}
+	// private SearchHit<Claim> parseClaimHit(HttpResponse response) {
+	// TODO Auto-generated method stub
+	// return null;
+	// }
 }
--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/View/NoSwipeViewPager.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/View/NoSwipeViewPager.java
@@ -1,23 +1,23 @@
 /*
-* Copyright 2015 Weijie Sun
-* Copyright 2015 Youdong Ma
-* Copyright 2015 Yufei Zhang
-* Copyright 2015 Chenrui Lei
-* Copyright 2015 Yang Zhang
-* Copyright 2015 Ji Yang
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 Weijie Sun
+ * Copyright 2015 Youdong Ma
+ * Copyright 2015 Yufei Zhang
+ * Copyright 2015 Chenrui Lei
+ * Copyright 2015 Yang Zhang
+ * Copyright 2015 Ji Yang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ca.ualberta.cs.cmput301w15t04team04project.View;
 
 import android.content.Context;
@@ -27,22 +27,27 @@ import android.view.MotionEvent;
 
 public class NoSwipeViewPager extends ViewPager {
 	public NoSwipeViewPager(Context context) {
-        super(context);
-    }
+		super(context);
+	}
 
-    public NoSwipeViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public NoSwipeViewPager(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent arg0) {
-        // Never allow swiping to switch between pages
-        return false;
-    }
+	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent arg0) {
+		// Never allow swiping to switch between pages
+		return false;
+	}
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        // Never allow swiping to switch between pages
-        return false;
-    }
+	/**
+	 * @Parameters event The motion event.
+	 * @Returns True if the event was handled, false otherwise.
+	 */
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// Never allow swiping to switch between pages
+		return false;
+	}
 }
