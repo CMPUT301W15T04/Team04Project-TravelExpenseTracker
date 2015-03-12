9b94e46b564d6b69359166489ca438787833c037
sonicsoy <jyang7@ualberta.ca>
2015-03-12 14:00:18 -0600

    correction

--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/FragmentEditItem1.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/FragmentEditItem1.java
@@ -13,7 +13,8 @@ public class FragmentEditItem1 extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_edit_item_1, container, false);
+		return inflater
+				.inflate(R.layout.fragment_edit_item_1, container, false);
 	}
 
 	@Override
