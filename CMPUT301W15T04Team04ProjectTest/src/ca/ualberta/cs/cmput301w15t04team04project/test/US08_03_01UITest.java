20819bcea530af49f021f8e2f2ae3ab84fa65b12
jeremykid <jeremykid1992@gmail.com>
2015-03-14 21:33:27 -0600

    itemdetail

--- CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/US08_03_01UITest.java
+++ CMPUT301W15T04Team04ProjectTest/src/ca/ualberta/cs/cmput301w15t04team04project/test/US08_03_01UITest.java
@@ -1,14 +1,21 @@
 package ca.ualberta.cs.cmput301w15t04team04project.test;
 
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.ListView;
+import ca.ualberta.cs.cmput301w15t04team04project.FragmentMoments;
+import ca.ualberta.cs.cmput301w15t04team04project.FragmentProfile;
 import ca.ualberta.cs.cmput301w15t04team04project.MainActivity;
 import junit.framework.TestCase;
 
 public class US08_03_01UITest extends ActivityInstrumentationTestCase2<MainActivity> {
 
 	private ListView claimlistview;
-	
+    private MainActivity thisActivity;
+	private FragmentProfile profilefragment;
+	private FragmentMoments momentfragment;
+    
 	public US08_03_01UITest() {
 		super(MainActivity.class);
 		// TODO Auto-generated constructor stub
@@ -17,13 +24,32 @@ public class US08_03_01UITest extends ActivityInstrumentationTestCase2<MainActiv
 	@Override
 	protected void setUp() throws Exception
 	{
-		getActivity();
 		super.setUp();
+		thisActivity = (MainActivity) getActivity();
+		profilefragment = (FragmentProfile) thisActivity.fragments.get(0);
+		
 	}
 	
+	public void testPreConditions() {
+        assertNotNull(thisActivity);
+        assertNotNull(momentfragment);
+        
+        
+    }
+	
 	protected void AllClaimDetailsUItest(){
-		
+		ListView listView = (ListView) thisActivity.findViewById(ca.ualberta.cs.cmput301w15t04team04project.R.id.myClaimsListView); //listView
+
 		
 		
 	}
+	
+/*	private Fragment startFragment(Fragment fragment) {
+	      FragmentTransaction transaction = mActivity.getSupportFragmentManager().beginTransaction();
+	      transaction.add(R.id.activity_test_fragment_linearlayout, fragment, "tag");
+	      transaction.commit();
+	      getInstrumentation().waitForIdleSync();
+	      Fragment frag = mActivity.getSupportFragmentManager().findFragmentByTag("tag");
+	      return frag;
+	    }*/
 }
