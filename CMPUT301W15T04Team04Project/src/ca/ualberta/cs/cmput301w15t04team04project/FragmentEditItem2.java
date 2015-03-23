8e7fda1ee73e23e64bf75eced57a1b1af112af73
jeremykid <jeremykid1992@gmail.com>
2015-03-22 21:51:03 -0600

    camera

--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/FragmentEditItem2.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/FragmentEditItem2.java
@@ -22,6 +22,7 @@ package ca.ualberta.cs.cmput301w15t04team04project;
 
 import java.io.File;
 
+
 import ca.ualberta.cs.cmput301w15t04team04project.CLmanager.MyLocalClaimListManager;
 import ca.ualberta.cs.cmput301w15t04team04project.models.ClaimList;
 import ca.ualberta.cs.cmput301w15t04team04project.models.Item;
@@ -31,6 +32,8 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
@@ -55,9 +58,12 @@ public class FragmentEditItem2 extends Fragment {
 	private int myItemId;
 	private int myClaimId;
 	private TextView itemDescription;
+	private View thisview;
+	private Uri imageFileUri;
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private Item currentItem;
+	private String stringUri = null;
 
-    private Uri imageFileUri;
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	
 	/**
 	 * This is the onCreateView of initial the view
@@ -70,23 +76,27 @@ public class FragmentEditItem2 extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-
 		// return inflater.inflate(R.layout.fragment_edit_item_2, container,
 		// false);
-		View view =inflater.inflate(R.layout.fragment_edit_item_2, container, false);        
-		ImageButton button = (ImageButton) view.findViewById(R.id.addRecieptImageButton);
-        OnClickListener listener = new OnClickListener() {
-            public void onClick(View v){
-                takeAPhoto();
-            }
-        };
-        button.setOnClickListener(listener);
+		View view = inflater.inflate(R.layout.fragment_edit_item_2, container,
+				false);
+		thisview = view;
+		ImageButton button = (ImageButton) view
+				.findViewById(R.id.addRecieptImageButton);
+		OnClickListener listener = new OnClickListener() {
+			public void onClick(View v) {
+				takeAPhoto();
+			}
+		};
+		button.setOnClickListener(listener);
 
 		return view;
 
 	}
+
 	/**
-	 * This is the onActivityCreated of create Item or Edit item set the original information in the View
+	 * This is the onActivityCreated of create Item or Edit item set the
+	 * original information in the View
 	 * 
 	 * @author Weijie Sun
 	 * @version 1.1
@@ -95,9 +105,9 @@ public class FragmentEditItem2 extends Fragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
-		ClaimList claimList = MyLocalClaimListManager.loadClaimList(
-				getActivity());
+
+		ClaimList claimList = MyLocalClaimListManager
+				.loadClaimList(getActivity());
 		Bundle bundle = getActivity().getIntent().getExtras();
 		if (bundle.size() == 1) {
 			EditItemActivity.addEditItemStatus = 0;
@@ -107,31 +117,65 @@ public class FragmentEditItem2 extends Fragment {
 			myItemId = bundle.getInt("myItemId");
 			Toast.makeText(getActivity(), "Frag ItemID = " + myItemId,
 					Toast.LENGTH_SHORT).show();
-			
-			Item currentItem = claimList.getClaimArrayList().get(myClaimId)
+
+			currentItem = claimList.getClaimArrayList().get(myClaimId)
 					.getItems().get(myItemId);
-			
-			itemDescription = (TextView) getView().findViewById(R.id.fragmentEditItem2DiscriptionEditText);
+
+			itemDescription = (TextView) getView().findViewById(
+					R.id.fragmentEditItem2DiscriptionEditText);
 			itemDescription.setText(currentItem.getItemDescription());
+			
+			/*ImageButton button = (ImageButton) thisview.findViewById(R.id.addRecieptImageButton);
+			imageFileUri = Uri.parse(currentItem.getReceipt());
+			button.setImageDrawable(Drawable.createFromPath(imageFileUri
+					.getPath()));*/
+		}
+
+	}
+
+	// resource from https://github.com/joshua2ua/BogoPicLab
+	public void takeAPhoto() {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		String folder = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/tmp";
+		File folderF = new File(folder);
+		if (!folderF.exists()) {
+			folderF.mkdir();
+		}
+
+		String imageFilePath = folder + "/"
+				+ String.valueOf(System.currentTimeMillis()) + "jpg";
+		File imageFile = new File(imageFilePath);
+		imageFileUri = Uri.fromFile(imageFile);
+		
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			if (resultCode == -1) {
+				
+				stringUri = imageFileUri.getPath();
+				Toast.makeText(getActivity(), stringUri,Toast.LENGTH_LONG ).show();
+				//currentItem.setReceipt(stringUri);
+				ImageButton button = (ImageButton) thisview.findViewById(R.id.addRecieptImageButton);
+				button.setImageDrawable(Drawable.createFromPath(imageFileUri
+						.getPath()));
+			}
 		}
 		
+		
 	}
-	
-	 public void takeAPhoto() {
-	        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	        
-	        String folder = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp";
-	        File folderF = new File(folder);
-	        if (!folderF.exists()) {
-	            folderF.mkdir();
-	        }
-	        
-	        String imageFilePath = folder + "/" + String.valueOf(System.currentTimeMillis()) + "jpg";
-	        File imageFile = new File(imageFilePath);
-	        imageFileUri = Uri.fromFile(imageFile);
-	        
-	        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
-	        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-	    }
+
+	public String getPath(Uri uri) {
+		String[] projection = { MediaStore.Images.Media.DATA };
+		Cursor cursor = getActivity().managedQuery(uri, projection, null, null, null);
+		int column_index = cursor
+				.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		cursor.moveToFirst();
+		return cursor.getString(column_index);
+	}
 }
 	
