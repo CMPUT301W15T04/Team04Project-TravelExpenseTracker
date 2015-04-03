b58b1673bc29f1daa3f33871be64882ee2b8ca04
jeremykid <jeremykid1992@gmail.com>
2015-04-03 14:35:17 -0600

    person mark

--- CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/osmMainActivity.java
+++ CMPUT301W15T04Team04Project/src/ca/ualberta/cs/cmput301w15t04team04project/osmMainActivity.java
@@ -1,23 +1,38 @@
 package ca.ualberta.cs.cmput301w15t04team04project;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+
+import ca.ualberta.cs.cmput301w15t04team04project.R.drawable;
 import ca.ualberta.cs.cmput301w15t04team04project.CLmanager.SignInManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.OverlayItem.HotspotPlace;
 
 
+
 public class osmMainActivity extends Activity {
 	private GeoPoint homeGeoPoint;
+	private ItemizedIconOverlay<OverlayItem> pointsOverlay;
+	private List<OverlayItem> points;
+	private ResourceProxy mResourceProxy;
+	private OverlayItem homeMark;
+	private MapView map;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +40,7 @@ public class osmMainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.osm_main);
-        MapView map = (MapView) findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
@@ -33,6 +48,29 @@ public class osmMainActivity extends Activity {
         mapController.setZoom(9);
         GeoPoint startPoint = new GeoPoint(48.8583, 2,2944);
         mapController.setCenter(startPoint);
+        
+		mResourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+		points = new ArrayList<OverlayItem>();
+        
+		homeMark = new OverlayItem("Home Location", "home", startPoint);
+		homeMark.setMarker(getResources().getDrawable(drawable.person));
+		
+		pointsOverlay = new ItemizedIconOverlay<OverlayItem>(points, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+
+			@Override
+			public boolean onItemLongPress(int arg0, OverlayItem arg1) {
+				return false;
+			}
+			@Override
+			public boolean onItemSingleTapUp(int arg0, OverlayItem arg1) {
+				return false;
+			}
+		},mResourceProxy);
+		
+		pointsOverlay.addItem(homeMark);
+		
+		map.getOverlays().add(pointsOverlay);
+
     }
 	
 	
